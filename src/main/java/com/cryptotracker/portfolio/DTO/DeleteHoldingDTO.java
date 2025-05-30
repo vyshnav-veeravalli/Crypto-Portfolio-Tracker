3b82ea965bea6c6b81f8f888e8f2a1cfc1f0e63a
Your Name <you@example.com>
2025-05-30 17:56:48 +0530

    Alertting system

---
+++ src/main/java/com/cryptotracker/portfolio/DTO/DeleteHoldingDTO.java
@@ -0,0 +1,13 @@
+package com.cryptotracker.portfolio.DTO;
+
+public class DeleteHoldingDTO {
+    private String symbol;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+}
